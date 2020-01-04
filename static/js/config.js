f5080987746d7c5f02d2e2b0907f27baf3cb8da2
Gil Tungol <gil.tungol@outlook.com>
2020-01-04 13:58:05 -0700

    Visualization-Project

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "a52bbbb87591b658b33e7a16c3415706";
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+// Perform an API call to Zomato
+var url = curl -X GET --header "Accept: application/json" --header "user-key: a52bbbb87591b658b33e7a16c3415706" "https://develo
+pers.zomato.com/api/v2.1/cities?q=Phoenix"
