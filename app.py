9c3a10fdd0b1645c621e4f6e5f9fd935ccc80fa3
Gil Tungol <gil.tungol@outlook.com>
2020-01-07 20:35:09 -0700

    Visualization-Project

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
 def hello():
-    message = "Hello, World"
+    message = "MKG"
     return render_template('index.html', message=message)
 
 # run the application
